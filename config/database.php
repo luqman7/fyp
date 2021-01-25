99353088fc2dc250a0e2239ed8a0dac0d5b4c91f
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-25 16:42:58 +0800

    initial

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', 'forge'),
-            // 'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => '/tmp/mysql.sock',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
